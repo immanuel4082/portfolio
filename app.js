7d05a1db5d1a598c3d53000a4b0924eed41a0023
immanuel4082 <immanuel4082@gmail.com>
2022-10-09 21:06:13 +0300

    script js

--- app.js
+++ app.js
@@ -35,3 +35,5 @@ function pageTransitions(){
 }
 
 pageTransitions();
+
+
---
+++ script.js
@@ -0,0 +1,8 @@
+//typing animation
+
+var typed = new Typed(".typing",{
+    strings:["Software Developer","ML Engineer","Data Scientist"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+}) 
